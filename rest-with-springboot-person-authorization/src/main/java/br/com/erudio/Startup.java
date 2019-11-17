2cff8513fae88e3d07f824978d40191ad86690cd
Alan Fernandes Meneguim <alan.meneguim@gmail.com>
2019-11-17 15:49:14 -0300

    Download e Upload de Arquivos

--- rest-with-springboot-person-authorization/src/main/java/br/com/erudio/Startup.java
+++ rest-with-springboot-person-authorization/src/main/java/br/com/erudio/Startup.java
@@ -3,10 +3,16 @@ package br.com.erudio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import br.com.erudio.config.FileStorageConfig;
+
 @SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageConfig.class
+})
 @EnableAutoConfiguration
 @ComponentScan
 public class Startup {
